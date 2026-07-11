8dbc11f01cacc5cea3fe6587a17e3d293b4b7e2b
Devendra Rathore <devendrarathorework@gmail.com>
2026-07-11 14:17:46 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const serverUrl = "https://chat-app-backend-v5pq.onrender.com" || "http://localhost:3000";
+export const serverUrl = "https://zero1-mern-chat-app.onrender.com" || "http://localhost:3000";
